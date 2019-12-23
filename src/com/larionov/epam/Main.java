5c0378dac4670ed3d5d824944920bdf25ce1a23b
Larionov <Dmitrii_Larionov@epam.com>
2019-12-23 12:05:12 +0400

    Chanage class Main and Caclculator

--- src/com/larionov/epam/Main.java
+++ src/com/larionov/epam/Main.java
@@ -6,6 +6,6 @@ import com.larionov.epam.service.impl.DemoServiceImpl;
 public class Main {
     public static void main(String[] args) {
         DemoService demoService = new DemoServiceImpl();
-        demoService.service("(40+50)*30+30/20+(40-30)");
+        demoService.service("(40+50)*30+300/20+(40-30)");
     }
 }
--- src/com/larionov/epam/service/Calculator.java
+++ src/com/larionov/epam/service/Calculator.java
@@ -2,7 +2,10 @@ package com.larionov.epam.service;
 
 public interface Calculator {
     String transferToRPN(String expr);
+
     boolean isNumber(String str);
+
     void calcRPN(String rpn);
+
     int getPriority(char token);
 }
